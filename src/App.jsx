11f9bbf862d8107f9ebfe225a4dc8e8ded7ddce9
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-02-16 10:12:50 +0530

    correct the folder structure of projct

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Loader from "./components/loader/Loader";
-import StartScreen from "./components/startscreen/StartScreen";
-import QuizScreen from "./components/quizscreen/QuizScreen";
-import ResultScreen from "./components/resultscreen/ResultScreen";
+import StartScreen from "./components/startScreen/StartScreen";
+import ResultScreen from "./components/resultScreen/ResultScreen";
 import { useQuiz } from "./context/QuizProvider";
 import QuizInfo from "./components/quizinfo/QuizInfo"
+import QuizScreen from "./components/quizScreen/QuizScreen";
+
 
 function App() {
   const { status } = useQuiz();
