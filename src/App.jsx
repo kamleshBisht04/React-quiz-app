3f9a793eccf121ae26ab7202f4bb0f7641e5936a
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-03-09 12:39:16 +0530

    correct the error from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ResultScreen from "./components/resultScreen/ResultScreen";
 import { useQuiz } from "./context/QuizProvider";
 import QuizInfo from "./components/quizInfo/QuizInfo"
 import QuizScreen from "./components/quizScreen/QuizScreen";
+import Error from "./components/ui/Error";
 
 
 function App() {
@@ -11,8 +12,10 @@ function App() {
   return (
     <main>
       {status === "loading" && <Loader />}
+      {status === "error" && <Error/>}
+
       {status === "ready" && <StartScreen />}
-      {status === "info" && <QuizInfo/>}
+      {status === "info" && <QuizInfo />}
       {status === "active" && <QuizScreen />}
       {status === "finished" && <ResultScreen />}
     </main>
